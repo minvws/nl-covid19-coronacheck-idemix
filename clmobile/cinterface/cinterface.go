b995880c25f5a9383269b5459ff8183e35c8c0a6
Ryan Barrett <ryan@seldonplan.com>
2021-04-07 15:46:47 +0200

    Updated the API to use GoString ala the issuer interface.

--- clmobile/cinterface/cinterface.go
+++ clmobile/cinterface/cinterface.go
@@ -7,8 +7,8 @@ import (
 )
 
 //export LoadIssuerPks
-func LoadIssuerPks(annotatedPksJson []byte, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
-	var r = clmobile.LoadIssuerPks(annotatedPksJson)
+func LoadIssuerPks(annotatedPksJson string, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
+	var r = clmobile.LoadIssuerPks([]byte(annotatedPksJson))
 	handleResult(r.Value, r.Error, resultBuffer, bufferLength, written, error)
 }
 
@@ -19,32 +19,32 @@ func GenerateHolderSk(resultBuffer unsafe.Pointer, bufferLength int, written *in
 }
 
 //export CreateCommitmentMessage
-func CreateCommitmentMessage(holderSkJson, issuerNonceMessageBase64 []byte, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
-	var r = clmobile.CreateCommitmentMessage(holderSkJson, issuerNonceMessageBase64)
+func CreateCommitmentMessage(holderSkJson, issuerNonceMessageBase64 string, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
+	var r = clmobile.CreateCommitmentMessage([]byte(holderSkJson), []byte(issuerNonceMessageBase64))
 	handleResult(r.Value, r.Error, resultBuffer, bufferLength, written, error)
 }
 
 //export CreateCredential
-func CreateCredential(holderSkJson, ccmJson []byte, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
-	var r = clmobile.CreateCredential(holderSkJson, ccmJson)
+func CreateCredential(holderSkJson, ccmJson string, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
+	var r = clmobile.CreateCredential([]byte(holderSkJson), []byte(ccmJson))
 	handleResult(r.Value, r.Error, resultBuffer, bufferLength, written, error)
 }
 
 //export ReadCredential
-func ReadCredential(credJson []byte, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
-	var r = clmobile.ReadCredential(credJson)
+func ReadCredential(credJson string, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
+	var r = clmobile.ReadCredential([]byte(credJson))
 	handleResult(r.Value, r.Error, resultBuffer, bufferLength, written, error)
 }
 
 //export DiscloseAllWithTimeQrEncoded
-func DiscloseAllWithTimeQrEncoded(holderSkJson, credJson []byte, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
-	var r = clmobile.DiscloseAllWithTimeQrEncoded(holderSkJson, credJson)
+func DiscloseAllWithTimeQrEncoded(holderSkJson, credJson string, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
+	var r = clmobile.DiscloseAllWithTimeQrEncoded([]byte(holderSkJson), []byte(credJson))
 	handleResult(r.Value, r.Error, resultBuffer, bufferLength, written, error)
 }
 
 //export DiscloseAllWithTime
-func DiscloseAllWithTime(credJson []byte, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
-	var r = clmobile.DiscloseAllWithTime(credJson)
+func DiscloseAllWithTime(credJson string, resultBuffer unsafe.Pointer, bufferLength int, written *int, error *bool) {
+	var r = clmobile.DiscloseAllWithTime([]byte(credJson))
 	handleResult(r.Value, r.Error, resultBuffer, bufferLength, written, error)
 }
 
